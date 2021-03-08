f0464b395c0649a14d0a3c013903aa18d8be9c11
Daniil Denisov <stxg9d@gmail.com>
2021-03-08 13:52:46 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
 module.exports = function reverse (n) {
-  let arr = n.toString().split('');
-  let reversed = arr.reverse();
-  let newNum =  Number(reversed.join('') * Math(n));
-  return newNum;
+  return parseFloat(n.toString().split('').reverse().join('')) * Math.sign(n);
 }
